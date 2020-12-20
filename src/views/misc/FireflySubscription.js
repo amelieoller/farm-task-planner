fd767d83e6a5089dc48aedefaec4189b4cfb6abe
Amelie Oller <amelieoller@gmail.com>
2020-12-19 18:11:34 -0800

    Change app name

--- src/views/misc/FireflySubscription.js
+++ src/views/misc/FireflySubscription.js
@@ -1,37 +1,37 @@
 // if the currently logged-in user is a paid subscriber,
-// this will provide their Firefly subscription
+// this will provide their Farmr subscription
 
-import React from 'react'
-import FirebaseAuth from './FirebaseAuth'
-import { FirestoreCollection } from 'react-firestore'
+import React from "react";
+import FirebaseAuth from "./FirebaseAuth";
+import { FirestoreCollection } from "react-firestore";
 
-const FireflySubscription = ({children}) => (
+const FarmrSubscription = ({ children }) => (
   <FirebaseAuth>
-    { ({isLoading, error, auth}) => {
-
+    {({ isLoading, error, auth }) => {
       if (error || isLoading || !auth) {
         return children({
           error,
           isLoading,
           subscription: null,
-        })
+        });
       }
 
-      return <FirestoreCollection
-        path="subscriptions"
-        filter={['createdBy', '==', auth.uid]}
-      >
-        { ({error, isLoading, data}) => {
-          return children({
-            error,
-            isLoading,
-            subscription: data.length > 0 ? data[0] : null,
-          })
-        }}
-      </FirestoreCollection>
-
+      return (
+        <FirestoreCollection
+          path="subscriptions"
+          filter={["createdBy", "==", auth.uid]}
+        >
+          {({ error, isLoading, data }) => {
+            return children({
+              error,
+              isLoading,
+              subscription: data.length > 0 ? data[0] : null,
+            });
+          }}
+        </FirestoreCollection>
+      );
     }}
   </FirebaseAuth>
-)
+);
 
-export default FireflySubscription
+export default FarmrSubscription;
